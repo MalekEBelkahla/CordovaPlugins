d83b658c7e764692ddebf481a0babb663f5ea279
MalekEBelkahla <malek.belkahla@gmail.com>
2015-08-11 16:26:48 +0100

    Create UUIDCreator.h

---
+++ UUIDCreator.h
@@ -0,0 +1,19 @@
+//
+//  UUIDCreator.h
+//  QIIBQIIBIphone
+//
+//  Created by Malek BelKahla on 10/08/2015.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import <Cordova/CDVPlugin.h>
+
+@interface UUIDCreator : CDVPlugin
+
+// read the UUID from keychain or UserDefaults
+- (void) readUUID:(CDVInvokedUrlCommand*)command;
+// write the UUID to keychain and UserDefaults
+- (void) writeUUID;
+
+@end
